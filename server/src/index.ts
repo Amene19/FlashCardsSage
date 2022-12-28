53eeff610332a2237b8fe262a1d2295dc4f0fd74
Amene19 <ameneallah.harhira@etudiant-fst.utm.tn>
2022-12-28 16:49:01 +0100

    commit

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,11 @@
 import { config } from 'dotenv';
 config();
-
 import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
-import Deck from "./models/Deck"
+import { getDecksController } from './controllers/getDecksController';
+import { createDeckController } from './controllers/createDeckController';
+import { deleteDeckController } from './controllers/deleteDeckController';
 
 const PORT = 5020;
 
@@ -15,26 +15,11 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get('/decks', async (req: Request, res: Response) => {
-    const decks = await Deck.find();
-    res.json(decks);
-})
+app.get('/decks', getDecksController);
 
-app.post('/decks', async (req: Request, res: Response)=>{
-    const newDeck = new Deck({
-        title: req.body.title,
-    });
-    const createdDeck = await newDeck.save();
-    res.json(createdDeck);
-    
-})
+app.post('/decks', createDeckController)
 
-app.delete('/decks/:deckId', async (req: Request,res: Response) => {
-    const deckId = req.params.deckId;
-    await Deck.findByIdAndDelete(deckId);
-    res.json();
-
-});
+app.delete('/decks/:deckId',deleteDeckController);
 
 
 
