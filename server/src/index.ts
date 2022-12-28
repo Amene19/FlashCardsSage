c7b2a42a5cbb86caafaace42369dc8d6f9069047
Amene19 <ameneallah.harhira@etudiant-fst.utm.tn>
2022-12-28 14:59:30 +0100

    working on showing the cards in the landing page

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ config();
 
 import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 
 import Deck from "./models/Deck"
 
@@ -10,8 +11,14 @@ const PORT = 5000;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
+
+app.get('/decks', async (req: Request, res: Response) => {
+    const decks = await Deck.find();
+    res.json(decks);
+})
 
 app.post('/decks', async (req: Request, res: Response)=>{
     const newDeck = new Deck({
